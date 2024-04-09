8093ea840f86b7510212ac01a9f7073b813078ca
Abdelhak AKRAM <abdelhak.akram@sii-maroc.com>
2024-04-09 13:50:31 +0000

    first commit

---
+++ src/main/java/com/example/liquibaseSpringBootPostgre/LiquibaseSpringBootPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibaseSpringBootPostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSpringBootPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseSpringBootPostgreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/liquibaseSpringBootPostgre/LiquibaseSpringBootPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquibaseSpringBootPostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseSpringBootPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
